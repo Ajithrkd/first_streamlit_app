50e059deeb96ee491a9660ae4face0f363a091b4
Ajith Moolayil Sasi <111120430+Ajithrkd@users.noreply.github.com>
2023-07-14 11:49:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Appam & Egg Curry')
+streamlit.text('Puttu & Kadala')
+streamlit.text('Porotta & Beef')
